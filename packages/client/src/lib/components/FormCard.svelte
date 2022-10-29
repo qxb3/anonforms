13d03d7a8f9c2f38b1fbfd270e63aa61777d8a2b
qxb3 <qxbthree@gmail.com>
2022-10-29 15:42:39 +0800

    added FormCard component

---
+++ packages/client/src/lib/components/FormCard.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let form
+</script>
+
+<div class="uk-padding-small uk-margin" style="border: .5px solid white; border-radius: 8px;">
+  <div style="display: flex; justify-content: space-between;">
+    <div>
+      <h4 class="uk-margin-remove">Title: {form.title}</h4>
+      <h6 class="uk-margin-remove">Description: {form.description}</h6>
+    </div>
+
+    <div>
+      <a href="/forms/{form.id}">
+        <i class="fa fa-arrow-right-to-bracket"></i>
+      </a>
+    </div>
+  </div>
+
+  <h6 class="uk-margin-top uk-margin-remove-bottom">Uploaded on: {form.uploadedDate.readable}</h6>
+</div>
